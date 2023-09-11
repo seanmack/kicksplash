e61b1604c7b1f2dd121fec09f8a5472c226bca77
Sean McEvoy <sfmcevoy@gmail.com>
2023-09-11 10:11:43 -0500

    Remove temporary spec now that CI is failing for the right reason

--- spec/temporary_spec.rb
+++ spec/temporary_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Temporary", type: :system do
-  # Spec fails for now so we can verify CI is working
-  it { should have_secure_password }
-
   it "should pass" do
     expect(true).to be true
   end
