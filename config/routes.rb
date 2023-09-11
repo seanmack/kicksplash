d5fb2f65724e2c7dd18da4af8eb4e110e920226e
Sean McEvoy <sfmcevoy@gmail.com>
2023-09-11 14:31:53 -0500

    User raw HTML and assets to render a static site based on CodeStitch demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # TODO: We'll start with static routes but move to RESTful routes soon
   scope controller: :static do
-    get :home
-    # get :about
-    # get :projects
-    # get :reviews
-    # get :blog
-    # get :contact
-    # get :example_blog_post
+    get :about
+    get :projects
+    get :reviews
+    get :blog
+    get :contact
   end
+
+  root "static#home", as: :home
 end
