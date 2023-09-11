e2bd778b5a7dcdde6a26e6a59d20f8174b86ff01
Sean McEvoy <sfmcevoy@gmail.com>
2023-09-11 16:34:27 -0500

    Temporary workaround to make it easier to port Eleventy site files

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,7 @@ module Kicksplash
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Temporary workaround to avoid changing URLs within existing static files
+    config.assets.prefix = "/temp_assets_directory"
   end
 end
